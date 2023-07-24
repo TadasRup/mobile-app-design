b8af37085fac8895aeed77cabc17b54189e1c1be
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-24 17:32:25 +0300

    css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { FirstPage } from './components/Page/FirstPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstPage />
+    </div>
+  );
+}
+
+export default App;
