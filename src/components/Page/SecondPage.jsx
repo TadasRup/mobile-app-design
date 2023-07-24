41f54ab87959e97ef5f4c01c1940e687f09aeec3
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-25 00:02:30 +0300

    changes

--- src/components/Page/SecondPage.jsx
+++ src/components/Page/SecondPage.jsx
@@ -1,13 +1,11 @@
-// SecondPage.js
-
 import React from 'react';
-import style2 from '../Page/SecondPage.module.css';
+import style from '../Page/SecondPage.module.css';
 
 const SecondPage = () => {
   return (
-    <div className={style2.secondpage}>
-      <h1 className={style2.message}>Are you first time in this app?</h1>
-      <button className={style2.continuebutton}>Continue</button>
+    <div className={style.secondpage}>
+      <h1 className={style.message}>Are you first time in this app?</h1>
+      <button className={style.continuebutton}>Continue</button>
     </div>
   );
 };
