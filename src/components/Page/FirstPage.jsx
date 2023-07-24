df1a95bc47215549e17981fd61eb85d8deefcbeb
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-24 19:11:10 +0300

    New design

--- src/components/Page/FirstPage.jsx
+++ src/components/Page/FirstPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import companyLogo from '../../img/logoapp2.png';
+import companyLogo from '../../img/backdoorblack.png';
 import style from '../Page/FirstPage.module.css';
 
 const FirstPage = () => {
   return (
     <div className={style.firstpage}>
       <img src={companyLogo} alt="Logo" className={style.logo} />
-      <h1 className={style.heading}>The largest trade-in app in the world!</h1>
+      <h1 className={style.heading}>Backdoor is the largest trade-in app in the world! </h1>
       <button className={style.getstartedbutton}>Get started now!</button>
     </div>
   );
