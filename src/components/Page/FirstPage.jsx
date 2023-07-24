e4591d1fb291ef071703b0b397baf0f6b0c3ace2
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-24 19:56:41 +0300

    Changed design

--- src/components/Page/FirstPage.jsx
+++ src/components/Page/FirstPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import companyLogo from '../../img/backdoorblack.png';
+import companyLogo from '../../img/techno.png';
 import style from '../Page/FirstPage.module.css';
 
 const FirstPage = () => {
   return (
     <div className={style.firstpage}>
       <img src={companyLogo} alt="Logo" className={style.logo} />
-      <h1 className={style.heading}>Backdoor is the largest trade-in app in the world! </h1>
+      <h1 className={style.heading}>A unique techno platform will allow you to discover undiscovered talents and share your creations</h1>
       <button className={style.getstartedbutton}>Get started now!</button>
     </div>
   );
