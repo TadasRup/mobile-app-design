ce78358923abcf1202130b2b11e02caaf7dd75f7
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-24 22:34:56 +0300

    FInal design

--- src/components/Page/FirstPage.jsx
+++ src/components/Page/FirstPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import companyLogo from '../../img/techno.png';
+import companyLogo from '../../img/prodtech.png';
 import style from '../Page/FirstPage.module.css';
 
 const FirstPage = () => {
   return (
     <div className={style.firstpage}>
       <img src={companyLogo} alt="Logo" className={style.logo} />
-      <h1 className={style.heading}>A unique techno platform will allow you to discover undiscovered talents and share your creations</h1>
+      <h1 className={style.heading}>Upload your tracks on the biggest music platform! You will be noticed by thousands of enthusiasts!</h1>
       <button className={style.getstartedbutton}>Get started now!</button>
     </div>
   );
