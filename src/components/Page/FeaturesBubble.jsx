2fd8399d8d3b0af119830be2f4b5f517ce278353
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-25 14:52:47 +0300

    bubbles

--- src/components/Page/FeaturesBubble.jsx
+++ src/components/Page/FeaturesBubble.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+// FeaturesBubble.jsx
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import style from './FeatureBubble.module.css';
 
-const FeatureBubble = ({ active }) => {
+const FeaturesBubble = () => {
+  const location = useLocation();
+  const [activeBubble, setActiveBubble] = useState(1);
+
+  useEffect(() => {
+    const featureNumber = Number(location.pathname.slice(-1));
+    setActiveBubble(featureNumber);
+  }, [location]);
+
   return (
-    <div
-      className={`${style.bubble} ${active ? style.active : ''}`}
-    ></div>
+    <div className={style.bubblesContainer}>
+      <div className={`${style.bubble} ${activeBubble === 1 ? style.active : ''}`} />
+      <div className={`${style.bubble} ${activeBubble === 2 ? style.active : ''}`} />
+      <div className={`${style.bubble} ${activeBubble === 3 ? style.active : ''}`} />
+    </div>
   );
 };
 
-export default FeatureBubble;
+export default FeaturesBubble;
