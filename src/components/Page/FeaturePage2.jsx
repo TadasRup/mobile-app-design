161bd768abc61e58e07972c6e6903da2200eb982
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-25 14:53:11 +0300

    FeaturePages

--- src/components/Page/FeaturePage2.jsx
+++ src/components/Page/FeaturePage2.jsx
@@ -1,15 +1,18 @@
 // FeaturePage2.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from "./FeaturePage2.module.css";
+import FeatureBubbles from './FeaturesBubble';
 
 const FeaturePage2 = () => {
   return (
-    <div>
-      <h1>Feature 2</h1>
-      <p>This is the second feature content.</p>
-      <Link to="/feature3">Continue</Link>
+    <div className={style.featurepage}>
+      <FeatureBubbles />
+      <h1 className={style.content}>Feature 2</h1>
+      <p className={style.content}>This is the first feature content.</p>
+      <Link className={style.continuebutton} to="/FeaturePage3">Continue</Link>
     </div>
-  );
-};
+  )
+}
 
 export default FeaturePage2;
--- src/components/Page/FeaturePage3.jsx
+++ src/components/Page/FeaturePage3.jsx
@@ -1,15 +1,18 @@
 // FeaturePage3.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style4 from './FeaturePage3.module.css'
+import FeatureBubbles from './FeaturesBubble';
 
 const FeaturePage3 = () => {
   return (
-    <div>
-      <h1>Feature 3</h1>
-      <p>This is the third feature content.</p>
-      <Link to="/CreateAccount">Continue</Link>
+    <div className={style4.featurepage}>
+      <FeatureBubbles />
+      <h1 className={style4.content}>Feature 3</h1>
+      <p className={style4.content}>This is the first feature content.</p>
+      <Link className={style4.continuebutton} to="/CreateAccount">Continue</Link>
     </div>
-  );
-};
+  )
+}
 
 export default FeaturePage3;
