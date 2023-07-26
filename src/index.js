8c22d9515ca1b15bab46d8ef98c3a67734b3e912
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-26 19:11:59 +0300

    github

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom'
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
+    <BrowserRouter BrowserRouter basename="/mobile-app-design">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
